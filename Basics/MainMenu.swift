bbb544d0be6574525f2a8750cbc72406d093a17e
Jolley71717 <duttonluke03@gmail.com>
2017-03-30 15:09:52 -0600

    added in initial sprite information

---
+++ Basics/MainMenu.swift
@@ -0,0 +1,17 @@
+//
+//  MainMenu.swift
+//  Basics
+//
+//  Created by Luke Dutton on 3/30/17.
+//  Copyright © 2017 LukeDutton. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+
+    override func didMove(to view: SKView) {
+        print("custom class running!")
+    }
+    
+}
